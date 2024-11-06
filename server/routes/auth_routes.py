907a9ae3dd88c529376af7bddb171c6d64cd2722
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-11-06 16:22:12 +0300

    routes

---
+++ server/routes/auth_routes.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+from controllers.auth_controller import auth_blueprint
+
+auth_routes = auth_blueprint
---
+++ server/routes/cart_routes.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+from controllers.cart_controller import cart_blueprint
+
+cart_routes = cart_blueprint
